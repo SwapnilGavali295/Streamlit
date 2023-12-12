03058bc3bf81b21bb3afe9bea9f1494bd77499c2
Swapnil Gavali <137003175+SwapnilGavali295@users.noreply.github.com>
2023-12-12 22:25:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def user_input_features():
     first_number = st.number_input("First number")
     second_number = st.number_input("Second number")
     third_number = st.number_input("Third number")
-    return max(first_number,second_number_third_number)
+    return max(first_number,second_number,third_number)
 
 x=user_input_features()
 st.subheader('The maximum among three numbers is:')
