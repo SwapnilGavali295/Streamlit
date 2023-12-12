bcb6d0b132fdd354147156ddda4161fa5debc68c
Swapnil Gavali <137003175+SwapnilGavali295@users.noreply.github.com>
2023-12-12 22:19:10 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.write("""
+# Largest number
+
+This app tells the largest number
+""")
+#Get Input
+
+st.header('Enter the numbers')
+
+def user_input_features():
+    first_number = st.number_input("First number")
+    second_number = st.number_input("Second number")
+    third_number = st.number_input("Third number")
+    return max(first_number,second_number_third_number)
+
+x=user_input_features()
+st.subheader('The maximum among three numbers is:')
+st.write(x)
